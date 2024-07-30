ff61da8946699486ecfdd33f7964430942dd1db5
Brian Martinez <132228607+brianprogh1@users.noreply.github.com>
2024-07-31 00:40:34 +0530

    Update 1.c

--- 1.c
+++ 1.c
@@ -1,29 +1,71 @@
-#include<stdio.h>
-int main()
-{
-int a[50],n,i,j,temp,min;
-printf("Enter the value of n:\n");
-scanf("%d",&n);
-printf("Enter elements in an array:\n",n);
-for(i=0;i<n;i++)
-scanf("%d", &a[i]);
-for(i = 0; i < n - 1; i++)
-{
-min=i;
-for(j = i + 1; j < n; j++)
-{
-if(a[min] > a[j])
-min=j;
+#include <stdio.h>
+struct Kruskal {
+    int parent[10]; 
+};
+int find(struct Kruskal *k, int m) {
+    int p = m;
+    while (k->parent[p] != 0)
+        p = k->parent[p];
+    return p;
 }
-if(min != i)
-{
-temp=a[i];
-a[i]=a[min];
-a[min]=temp;
+
+void union_set(struct Kruskal *k, int i, int j) {
+    if (i < j)
+        k->parent[i] = j;
+    else
+        k->parent[j] = i;
 }
+
+void kruskal_algorithm(int a[10][10], int n) {
+    struct Kruskal k;
+    int u, v, min, k_count = 0, i, j, sum = 0;
+
+    for (i = 1; i <= n; i++)
+        k.parent[i] = 0;
+
+    while (k_count < n - 1) {
+        min = 99; 
+        for (i = 1; i <= n; i++) {
+            for (j = 1; j <= n; j++) {
+                if (a[i][j] < min && i != j) {
+                    min = a[i][j];
+                    u = i;
+                    v = j;
+                }
+            }
+        }
+
+        i = find(&k, u);
+        j = find(&k, v);
+
+        if (i != j) {
+            union_set(&k, i, j);
+            printf("(%d,%d)=%d\n", u, v, a[u][v]);
+            sum += a[u][v];
+            k_count++;
+        }
+
+        a[u][v] = a[v][u] = 99;
+    }
+
+    printf("The cost of minimum spanning tree = %d\n", sum);
 }
-printf("Sorted Array:\n");
-for(i = 0; i < n; i++)
-printf("%d\n", a[i]);
-return 0;
+
+int main() {
+    int a[10][10];
+    int i, j, n;
+
+    printf("Enter the number of vertices of the graph: ");
+    scanf("%d", &n);
+
+    printf("Enter the weighted matrix:\n");
+    for (i = 1; i <= n; i++) {
+        for (j = 1; j <= n; j++) {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    kruskal_algorithm(a, n);
+
+    return 0;
 }
